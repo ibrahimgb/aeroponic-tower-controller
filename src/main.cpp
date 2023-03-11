d8516392bd2777cb3e59e442876a73ffc12f70c9
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-03-11 17:41:59 +0100

    initial setup

---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+
+const char* ssid = "Tenda_423FF0";
+const char* password = "JBM120756";
+
+
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+   Serial.begin(9600);
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Setup done");
+  initWiFi();
+  Serial.println("wifi connected");
+}
+
+void loop() {
+    Serial.println("scan start");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+      Serial.println("no networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  /*
+  if (WiFi.status() != WL_DISCONNECTED){
+    Serial.println("wifi is Disconected");
+    initWiFi();
+  }
+  */
+Serial.println(WiFi.status());
+  // Wait a bit before scanning again
+  delay(2000);
+
+}
